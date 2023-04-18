0355873026ef7219d9267082530a414ba48856cb
Vishal Pal <vishal@zonvoir.com>
2023-04-18 18:21:42 +0530

    initial commits

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <MainHeader></MainHeader>
+  <div id="game">
+    <section id="monster" class="container">
+      <h2>Monster Health</h2>
+      <div class="healthbar">
+        <div class="healthbar__value"></div>
+      </div>
+    </section>
+    <section id="player" class="container">
+      <h2>Your Health</h2>
+      <div class="healthbar">
+        <div class="healthbar__value"></div>
+      </div>
+    </section>
+    <section id="controls">
+      <button>ATTACK</button>
+      <button>SPECIAL ATTACK</button>
+      <button>HEAL</button>
+      <button>SURRENDER</button>
+    </section>
+    <section id="log" class="container">
+      <h2>Battle Log</h2>
+      <ul></ul>
+    </section>
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue';
+
+export default {
+  components: {
+    MainHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <h1>Monster Slayer</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
